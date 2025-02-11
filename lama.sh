c595a178e9c0d39fdd2a4540eafd78ed3ecf4a49
Aisbir Never Sad <aisbirkun@gmail.com>
2025-02-11 14:27:18 +0700

    Create lama.sh

---
+++ lama.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt install -y figlet && \
+sudo chmod -x /etc/update-motd.d/* && \
+sudo curl -o /etc/update-motd.d/00-custom https://raw.githubusercontent.com/sumshiiy/ubuntu-motd/refs/heads/main/login.sh && \
+sudo chmod +x /etc/update-motd.d/00-custom
+echo "Done"
