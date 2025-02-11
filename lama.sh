96592f0b10b02ce856c9c83161f6fe0b413fa703
Aisbir Never Sad <aisbirkun@gmail.com>
2025-02-11 14:30:33 +0700

    Update lama.sh

--- lama.sh
+++ lama.sh
@@ -2,4 +2,4 @@ sudo apt install -y figlet && \
 sudo chmod -x /etc/update-motd.d/* && \
 sudo curl -o /etc/update-motd.d/00-custom https://raw.githubusercontent.com/sumshiiy/ubuntu-motd/refs/heads/main/login.sh && \
 sudo chmod +x /etc/update-motd.d/00-custom
-echo "Done"
+echo "Done Try Relogin On your Server"
